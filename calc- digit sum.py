8a58e69e31a8f643057d1a8db5c5762aa177fd16
gibler1 <86534336+gibler1@users.noreply.github.com>
2022-12-06 23:28:54 +0000

    Add files via upload

---
+++ calc- digit sum.py
@@ -0,0 +1,6 @@
+nm = 3*3333333*3333333
+num = str(nm)
+sum=0
+for i in num:
+    sum =sum + int(i)
+print(sum)
---
+++ e.py
@@ -0,0 +1,6 @@
+n=1
+while True:
+    
+    e=float((1+(1/n))**n)
+    n+=1
+    print(e)
